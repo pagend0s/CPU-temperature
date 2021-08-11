f6d637fb8faec007c4c3c257d74b5cef08002766
pagend0s <65870887+pagend0s@users.noreply.github.com>
2021-08-11 13:37:38 +0200

    Add files via upload

---
+++ temp_check.ps1
@@ -0,0 +1,28 @@
+﻿
+function Get-Temperature {
+    $t = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+    Write-Output $t
+    
+    $returntemp = @()
+
+    foreach ($temp in $t.CurrentTemperature)
+    {
+
+    $currentTempKelvin = $temp / 10
+    $currentTempCelsius = $currentTempKelvin - 273.15
+
+    $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+
+    $returntemp += $currentTempCelsius.ToString() + " C : " + $currentTempFahrenheit.ToString() + " F : " + $currentTempKelvin + "K"  
+    }
+    cls
+    $t = $null
+    $temp = $null
+    return $returntemp
+    $returntemp = $null    
+    
+}
+for (;;){
+Get-Temperature
+Start-Sleep -Seconds 2
+}
